e694baee15afa0e5c35f12fd6ec77a187eb43dbb
Moein <122750743+yolowex@users.noreply.github.com>
2024-04-13 22:28:55 +0330

    Removed redundant modifiers from SettingsConstants

--- service/src/main/java/com/slipkprojects/ultrasshservice/config/SettingsConstants.java
+++ service/src/main/java/com/slipkprojects/ultrasshservice/config/SettingsConstants.java
@@ -4,7 +4,7 @@ public interface SettingsConstants
 {
 	
 	// Geral
-	public static final String
+	String
 		AUTO_CLEAR_LOGS_KEY = "autoClearLogs",
 		HIDE_LOG_KEY = "hideLog",
 		MODE_DEBUG_KEY = "modeDebug",
@@ -26,43 +26,43 @@ public interface SettingsConstants
 		PROXY_USE_AUTHENTICATION_KEY = "usarProxyAutenticacao"
 	;
 	
-	public static final String
-			CONFIG_PROTECT_KEY = "protegerConfig",
-			CONFIG_MESSAGE_KEY = "mensagemConfig",
-			CONFIG_VALIDITY_KEY = "validadeConfig",
-			CONFIG_EXPORT_MESSAGE_KEY = "mensagemConfigExport",
-			CONFIG_INPUT_PASSWORD_KEY = "inputPassword"
+	String
+		CONFIG_PROTECT_KEY = "protegerConfig",
+		CONFIG_MESSAGE_KEY = "mensagemConfig",
+		CONFIG_VALIDITY_KEY = "validadeConfig",
+		CONFIG_EXPORT_MESSAGE_KEY = "mensagemConfigExport",
+		CONFIG_INPUT_PASSWORD_KEY = "inputPassword"
 	;
 
 	// Vpn
-	public static final String
-			DNS_FORWARD_KEY = "dnsForward",
+	String
+		DNS_FORWARD_KEY = "dnsForward",
 		DNS_RESOLVER_KEY = "dnsResolver",
 		UDP_FORWARD_KEY = "udpForward",
 		UDP_RESOLVER_KEY = "udpResolver";
 
 	// SSH
-	public static final String
-			SSH_SERVER_ADDRESS = "sshServer",
-			SSH_SERVER_PORT = "sshPort",
-			SSH_USER = "sshUser",
-			SSH_PASS = "sshPass",
-	KEY_PATH_KEY = "keyPath",
-	LOCAL_PORT_KEY = "sshPortaLocal",
-	PING_KEY = "pingerSSH";
+	String
+		SSH_SERVER_ADDRESS = "sshServer",
+		SSH_SERVER_PORT = "sshPort",
+		SSH_USER = "sshUser",
+		SSH_PASS = "sshPass",
+		KEY_PATH_KEY = "keyPath",
+		LOCAL_PORT_KEY = "sshPortaLocal",
+		PING_KEY = "pingerSSH";
 	
-	public static final String
+	String
 		PAYLOAD_DEFAULT = "CONNECT [host_port] [protocol][crlf][crlf]";
 
 	// Tunnel Type
-	public static final String
-			TUNNEL_TYPE_KEY = "tunnelType",
-			TUNNEL_TYPE_SSH_DIRECT = "sshDirect",
-			TUNNEL_TYPE_SSH_PROXY = "sshProxy",
-			TUNNEL_TYPE_SSH_SSL_TUNNEL = "sshSslTunnel";
+	String
+		TUNNEL_TYPE_KEY = "tunnelType",
+		TUNNEL_TYPE_SSH_DIRECT = "sshDirect",
+		TUNNEL_TYPE_SSH_PROXY = "sshProxy",
+		TUNNEL_TYPE_SSH_SSL_TUNNEL = "sshSslTunnel";
 
 	
-	public static final int
+	int
 		bTUNNEL_TYPE_SSH_DIRECT = 1,
 		bTUNNEL_TYPE_SSH_PROXY = 2
 	;
